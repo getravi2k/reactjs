58af8d14fe0e64399232c6d8006fa1693b532113
Nandam, Ravindra <ranandam@visa.com>
2016-10-25 23:50:39 -0700

    init

---
+++ test.js
@@ -0,0 +1,7 @@
+var rs = require('readline-sync');
+
+var aNewNum = 30;
+var aNewNewNum = 20;
+console.log(aNewNum + aNewNewNum);
+var name = rs.question('What is your name?');
+console.log("The user name is "+name);
